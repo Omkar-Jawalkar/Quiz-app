bab0142465b0b9c99e3afb905cfda4b1e4913786
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2024-06-11 08:08:40 +0530

    Bypass added

--- src/components/model/NameModel.js
+++ src/components/model/NameModel.js
@@ -1,19 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  Button,
-  ModalBody,
-  Text,
-  Input,
-  ModalCloseButton,
-  useDisclosure,
-  Flex,
-  Divider,
-  useToast,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    Button,
+    ModalBody,
+    Text,
+    Input,
+    ModalCloseButton,
+    useDisclosure,
+    Flex,
+    Divider,
+    useToast,
 } from "@chakra-ui/react";
 import validateEmail from "../../utils/index";
 import { doc, setDoc } from "firebase/firestore";
@@ -22,111 +22,118 @@ import { MyContext } from "@/context/myContext";
 import { useContext } from "react";
 import { getFirestore } from "firebase/firestore";
 const NameModel = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const db = getFirestore(app);
-  const initialRef = React.useRef();
-  const finalRef = React.useRef();
-  const toast = useToast();
-  const { user, setUser } = useContext(MyContext);
-  const OverlayOne = () => (
-    <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(5px)" />
-  );
-  useEffect(() => {
-    if (user.length < 6) {
-      onOpen();
-    }
-  }, []);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const db = getFirestore(app);
+    const initialRef = React.useRef();
+    const [email, setEmail] = useState("testing@gmail.com");
+    const finalRef = React.useRef();
+    const toast = useToast();
+    const { user, setUser } = useContext(MyContext);
+    const OverlayOne = () => (
+        <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(5px)" />
+    );
+    useEffect(() => {
+        if (user.length < 6) {
+            onOpen();
+        }
+    }, []);
 
-  // This is for validating email
+    // This is for validating email
 
-  return (
-    <Modal
-      initialFocusRef={initialRef}
-      finalFocusRef={finalRef}
-      isOpen={isOpen}
-      onClose={onClose}
-      isCentered
-      blockScrollOnMount={true}
-      closeOnOverlayClick={false}
-    >
-      <OverlayOne />;
-      <ModalContent>
-        <ModalHeader>Please Enter your Email 🙇‍♂️</ModalHeader>
-        <ModalBody>
-          <Flex
-            direction={"column"}
-            gap={2}
-            justifyContent={"center"}
-            textAlign={"center"}
-          >
-            <Text fontSize={"lg"} as="p">
-              Hey, would you mind entering your Email? 🙋‍♂️
-            </Text>
-            <Divider />
-            <Input
-              autoComplete="off"
-              id="user_name"
-              ref={initialRef}
-              placeholder="Email"
-            />
-          </Flex>
-        </ModalBody>
-        <ModalFooter>
-          <Flex w={"full"}>
-            <Button
-              onClick={async (e) => {
-                e.preventDefault();
-                const name = document.getElementById("user_name").value;
-                // console.log(name);
-                const val = validateEmail(name);
-                // console.log("val", val);
-                if (val === false) {
-                  toast({
-                    title: "Please enter a valid Email",
-                    status: "warning",
-                    isClosable: true,
-                    duration: 3000,
-                  });
-                  return;
-                } else {
-                  try {
-                    await setDoc(
-                      doc(db, "Users", name),
-                      { capital: true },
-                      { merge: true }
-                    );
-                    toast({
-                      title: " Success Login 🎉 ",
-                      status: "success",
-                      duration: 3000,
-                      isClosable: true,
-                    });
-                    localStorage.setItem("user_name", name);
-                    setUser(name);
-                    onClose();
-                  } catch (e) {
-                    console.error("Error adding document: ", e);
-                    toast({
-                      title: "Error adding document: ",
-                      status: "error",
-                      duration: 3000,
-                      isClosable: true,
-                    });
-                    onClose();
-                  }
-                }
-              }}
-              textAlign={"center"}
-              w={"full"}
-              colorScheme="blue"
-            >
-              Save
-            </Button>
-          </Flex>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  );
+    return (
+        <Modal
+            initialFocusRef={initialRef}
+            finalFocusRef={finalRef}
+            isOpen={isOpen}
+            onClose={onClose}
+            isCentered
+            blockScrollOnMount={true}
+            closeOnOverlayClick={false}
+        >
+            <OverlayOne />;
+            <ModalContent>
+                <ModalHeader>Please Enter your Email 🙇‍♂️</ModalHeader>
+                <ModalBody>
+                    <Flex
+                        direction={"column"}
+                        gap={2}
+                        justifyContent={"center"}
+                        textAlign={"center"}
+                    >
+                        <Text fontSize={"lg"} as="p">
+                            Hey, would you mind entering your Email? 🙋‍♂️
+                        </Text>
+                        <Divider />
+                        <Input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            autoComplete="off"
+                            id="user_name"
+                            ref={initialRef}
+                            placeholder="Email"
+                        />
+                    </Flex>
+                </ModalBody>
+                <ModalFooter>
+                    <Flex w={"full"}>
+                        <Button
+                            onClick={async (e) => {
+                                e.preventDefault();
+                                const name = email;
+                                // document.getElementById("user_name").value;
+                                // console.log(name);
+                                const val = validateEmail(name);
+                                // console.log("val", val);
+                                if (val === false) {
+                                    toast({
+                                        title: "Please enter a valid Email",
+                                        status: "warning",
+                                        isClosable: true,
+                                        duration: 3000,
+                                    });
+                                    return;
+                                } else {
+                                    try {
+                                        await setDoc(
+                                            doc(db, "Users", name),
+                                            { capital: true },
+                                            { merge: true }
+                                        );
+                                        toast({
+                                            title: " Success Login 🎉 ",
+                                            status: "success",
+                                            duration: 3000,
+                                            isClosable: true,
+                                        });
+                                        localStorage.setItem("user_name", name);
+                                        setUser(name);
+                                        onClose();
+                                    } catch (e) {
+                                        console.error(
+                                            "Error adding document: ",
+                                            e
+                                        );
+                                        toast({
+                                            title: "Error adding document: ",
+                                            status: "error",
+                                            duration: 3000,
+                                            isClosable: true,
+                                        });
+                                        onClose();
+                                    }
+                                }
+                            }}
+                            textAlign={"center"}
+                            w={"full"}
+                            colorScheme="blue"
+                        >
+                            Save
+                        </Button>
+                    </Flex>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    );
 };
 
 export default NameModel;
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -8,18 +8,26 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_authDomain,
-  projectId: process.env.NEXT_PUBLIC_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_appId,
-  measurementId: process.env.NEXT_PUBLIC_measurementId,
+    apiKey: process.env.NEXT_PUBLIC_apiKey,
+    authDomain: process.env.NEXT_PUBLIC_authDomain,
+    projectId: process.env.NEXT_PUBLIC_projectId,
+    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+    appId: process.env.NEXT_PUBLIC_appId,
+    measurementId: process.env.NEXT_PUBLIC_measurementId,
 };
 
-// console.log(firebaseConfig);
+console.log(firebaseConfig);
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
+let app;
+if (getApps().length < 1) {
+    app = initializeApp(firebaseConfig);
+} else {
+    app = getApps()[0];
+}
+
+// const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
